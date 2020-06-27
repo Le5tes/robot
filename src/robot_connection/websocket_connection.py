20c2e2a526306acb71a9c21384c8b1c2f7bd91be
Tim Williamson <timwilliamson1337@gmail.com>
2020-06-27 10:37:01 +0100

    add logging, update connection and add stop command

--- src/robot_connection/websocket_connection.py
+++ src/robot_connection/websocket_connection.py
@@ -1,13 +1,14 @@
 import asyncio
 
 def getControlConnection(controller): 
-    def controlConnection(websocket, path):
+    async def controlConnection(websocket, path):
         escape = False
         while not escape:
             command = await websocket.recv()
             if command == "END":
                 escape = True
             else:
+                print("command")
                 websocket.send(controller.giveCommand(command))
     return controlConnection
 
--- src/robot_control/robot_control.py
+++ src/robot_control/robot_control.py
@@ -5,6 +5,7 @@ class RobotController:
         self.robot = gpiozero.Robot(left=(17,18), right=(27,22))
 
     def giveCommand(self, command):
+        print(command)
         if command == "forward":
             self.robot.forward()
         elif command == "back":
@@ -13,6 +14,8 @@ class RobotController:
             self.robot.right()
         elif command == "left":
             self.robot.left()
+        elif command == "stop":
+            self.robot.stop()
         else:
             return "Command not recognised"
         return command
