11132cee209a7abe0cab5157544f448e8b3396dc
Tim Williamson <timothy.williamson@santander.co.uk>
2020-08-18 15:58:30 +0100

    add camera connection

---
+++ src/camera_connection/camera_connection.py
@@ -0,0 +1,32 @@
+from camera_connection.camera_converter_output import CameraConverterOutput
+from time import sleep
+import picamera
+import asyncio
+
+WIDTH = 640
+HEIGHT = 480
+FRAMERATE = 24
+VFLIP = False
+HFLIP = False
+
+async def broadcastOut(converter, websocket):
+    try:
+        while True:
+            buf = await converter.stdout.read1(32768)
+            if buf:
+                websocket.send(buf)
+            elif converter.poll() is not None:
+                break
+    finally:
+        converter.stdout.close()
+
+def startCamera(websocket):
+    with picamera.PiCamera() as camera:
+        camera.resolution = (WIDTH, HEIGHT)
+        camera.framerate = FRAMERATE
+        camera.vflip = VFLIP # flips image rightside up, as needed
+        camera.hflip = HFLIP # flips image left-right, as needed
+        sleep(1) # camera warm-up time
+        output = CameraConverterOutput(camera)
+        broadcastOut(output.converter, websocket)
+        camera.start_recording(output, 'yuv')
--- src/robot_connection/websocket_connection.py
+++ src/robot_connection/websocket_connection.py
@@ -1,7 +1,9 @@
 import asyncio
+from camera_connection.camera_connection import startCamera
 
 def getControlConnection(controller): 
     async def controlConnection(websocket, path):
+        startCamera(websocket)
         escape = False
         while not escape:
             command = await websocket.recv()
